8bffc018988884625ecf1ecd56a92bca8a062e78
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-15 15:29:44 -0400

    used MongoClient to connect to the database using the uri variable

--- server.js
+++ server.js
@@ -2,16 +2,18 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
+const { url } = require('node:inspector');
 const PORT = process.env.PORT;
-const uri = process.env.MONGODB_URI
+const uri = process.env.MONGODB_URI;
 
 // MIDDLEWARE
+const client = new MongoClient(uri);
 
 // ROUTES
 app.get("/", (req, res) => {
-    res.send("Test Route")
-})
+    res.send("Test Route");
+});
 
 // PORT
 app.listen(PORT, () => {
