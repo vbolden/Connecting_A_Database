49d38f3a5346401e567fdac47f2c2842b4ea0fea
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-15 15:36:25 -0400

    removed unused import

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
-const { url } = require('node:inspector');
 const PORT = process.env.PORT;
 const uri = process.env.MONGODB_URI;
 
